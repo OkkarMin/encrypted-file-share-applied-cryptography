ecec1ec5d26761e7b8745d9d2564aceeb92942a8
Okkar Min <OMIN001@e.ntu.edu.sg>
2021-10-27 09:08:55 +0800

    refactor: :recycle: Refactor socket.ts for eaiser readibility

--- nextjs-client/src/utils/socket.ts
+++ nextjs-client/src/utils/socket.ts
@@ -2,6 +2,7 @@ import io, { Socket } from "socket.io-client";
 import { encrypt, decrypt } from "./cryptography";
 
 import { IMessageObject } from "../../interface";
+import { MenuOptionGroup } from "@chakra-ui/menu";
 
 let socket: Socket;
 let key: string = "defaultKey";
@@ -31,16 +32,18 @@ const subscribeToChat = (cb: Function) => {
   socket.on("chat", (messageObject: IMessageObject) => {
     console.log("Websocket event received!");
 
-    let decryptedMessageObject = messageObject;
-
     let plainBody: string;
     try {
       plainBody = decrypt(messageObject.body, key);
     } catch (error) {
+      // if the key is wrong, we don't want to decrypt the message
       plainBody = undefined;
     }
 
-    decryptedMessageObject = { ...decryptedMessageObject, body: plainBody };
+    const decryptedMessageObject = {
+      ...messageObject,
+      body: plainBody,
+    };
 
     return plainBody
       ? cb(null, decryptedMessageObject)
@@ -51,11 +54,9 @@ const subscribeToChat = (cb: Function) => {
 const sendMessage = (room: string, messageObject: IMessageObject) => {
   if (!socket) return true;
 
-  let encryptedMessageObject = messageObject;
-
   const cipherBody = encrypt(messageObject.body, key);
 
-  encryptedMessageObject = { ...encryptedMessageObject, body: cipherBody };
+  const encryptedMessageObject = { ...messageObject, body: cipherBody };
 
   socket.emit("chat", { messageObject: encryptedMessageObject, room });
 };
